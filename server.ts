9a20f4076adab346ce5ca65c940255045ce912af
pevidex <ricardojosexavier@hotmail.com>
2021-06-11 15:01:26 +0100

    refactor: remove bodyParser dependency

--- server.ts
+++ server.ts
@@ -1,15 +1,13 @@
-const express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000,
-  bodyParser = require('body-parser')
-
+import express from 'express';
 import routes from './src/api/routes/uploadRoutes'; //importing route
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const app = express(),
+      port = process.env.PORT || 3000
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 routes(app); //register the route
-
 app.listen(port);
 
 console.log('Listenning on: ' + port);
